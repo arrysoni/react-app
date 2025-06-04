1e363b1c9fd64bb44aae1d59f2fd5c948b37b6cc
Aarya Soni <aarya19204@gmail.com>
2025-06-04 17:01:58 +0530

    Added a reactive button

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,41 @@
 // import ListGroup from "./components/ListGroup";
 
-// import Alert from "./components/Alert";
-import Button from "./components/Button";
+// // Each component in React has its own state. So in case we add one more  <ListGroup />, it will not act as the same as the first  <ListGroup />
 
-function App() {
-  // // Each component in React has its own state. So in case we add one more  <ListGroup />, it will not act as the same as the first  <ListGroup />
+// let items = [
+//   "New York",
+//   "San Francisco",
+//   "Philadelphia",
+//   "Los Angeles",
+//   "Paris",
+// ];
 
-  // let items = [
-  //   "New York",
-  //   "San Francisco",
-  //   "Philadelphia",
-  //   "Los Angeles",
-  //   "Paris",
-  // ];
+// Event handler for SelectItem
+// const handleSelectItem = (item: string) => {
+//   console.log(item);
+// };
 
-  // Event handler for SelectItem
-  // const handleSelectItem = (item: string) => {
-  //   console.log(item);
-  // };
-
-  return (
-    <div>
-      {
-        /* <ListGroup
+/* <ListGroup
         items={items}
         heading="Cities"
         onSelectItem={handleSelectItem}
       /> */
 
-        // <Alert>Hello World!</Alert>
+import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
 
-        <Button color="danger" onClick={() => console.log("Clicked")}>
-          Aarya's Button
-        </Button>
-      }
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        Aarya's Button
+      </Button>
     </div>
   );
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,10 +2,22 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode; // This allows for the child of any type!!!
+  onClose: () => void;
 }
 
-const Alert = ({ children }: Props) => {
-  return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, onClose }: Props) => {
+  return (
+    <div className="alert alert-warning alert-dismissible">
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        onClick={onClose}
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
+    </div>
+  );
 };
 
 export default Alert;
