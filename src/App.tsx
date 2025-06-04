589f26dfbcdb9c5634bec51b06359c110f04f3ba
Aarya Soni <aarya19204@gmail.com>
2025-06-04 12:35:14 +0530

    Building Components: Managing States

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  // Each component in React has its own state. So in case we add one more  <ListGroup />, it will not act as the same as the first  <ListGroup />
   return (
     <div>
       <ListGroup />
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -39,7 +39,8 @@
 // We want to render the list now, but since JavaScript doesn't allow for loops,n we use a function called map. We convert one data type into another.
 // Since the map function isn't allowed in the middle of the code, or under any other component, wrap the map function using '{}'. We can also use a method here as shown in the code.
 
-import { MouseEvent } from "react";
+// import { MouseEvent } from "react";
+import { useState } from "react";
 function ListGroup() {
   let items = [
     "New York",
@@ -49,9 +50,12 @@ function ListGroup() {
     "Paris",
   ];
 
+  // Hook: This is how we tell react that we our selectedIndex can have data or value that can change overtime.
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   // For more complex logic, move the logic to a different funtcion for event handling. Since the type of event is not explicitly defined here, we hover over the 'event' in our return statement and get to know the type for itl, i.e., MouseEvent. We therefore, import the MouseEvent.
   // Event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
+  // const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
@@ -59,11 +63,18 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li
-            className="list-group-item"
+          <li // Rendering classes dynamically
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
             // onClick={() => console.log(item, index)} // When the element of the list is clicked on our webpage, we will see the item, index on the console (Inspect/ Console)
-            onClick={handleClick} // On console, we get the type of the object as SyntheticBaseEvent which is a built in classes in React because different browsers have different implementations of event objects, so to make this cross browser, we have this class SyntheticBaseEvent (a wrapper around the native browser event object)
+            // onClick={handleClick} // On console, we get the type of the object as SyntheticBaseEvent which is a built in classes in React because different browsers have different implementations of event objects, so to make this cross browser, we have this class SyntheticBaseEvent (a wrapper around the native browser event object)
+            onClick={() => {
+              setSelectedIndex(index); // Despite making these changes, we are not able to get a highlighted version of our item. This is because our selectedIndex = 0 in local to our code. In order for it to change values when we click on a certain item, we need to use a 'Hook' called useState and import it as well.
+            }}
           >
             {item}
           </li> // It is necessary to give each element of the list a unique key, since in our list each sting is unique, we go for key as the item itself.
