19c5e34c6341ee26a409acf5588591ec62bfd40e
Aarya Soni <aarya19204@gmail.com>
2025-06-04 14:49:02 +0530

    Building Components: Passing Children

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
-import ListGroup from "./components/ListGroup";
+// import ListGroup from "./components/ListGroup";
+
+import Alert from "./components/Alert";
 
 function App() {
-  // Each component in React has its own state. So in case we add one more  <ListGroup />, it will not act as the same as the first  <ListGroup />
+  // // Each component in React has its own state. So in case we add one more  <ListGroup />, it will not act as the same as the first  <ListGroup />
 
-  let items = [
-    "New York",
-    "San Francisco",
-    "Philadelphia",
-    "Los Angeles",
-    "Paris",
-  ];
+  // let items = [
+  //   "New York",
+  //   "San Francisco",
+  //   "Philadelphia",
+  //   "Los Angeles",
+  //   "Paris",
+  // ];
 
   // Event handler for SelectItem
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
+
   return (
     <div>
-      <ListGroup
+      {
+        /* <ListGroup
         items={items}
         heading="Cities"
         onSelectItem={handleSelectItem}
-      />
+      /> */
+
+        <Alert>Hello World!</Alert>
+      }
     </div>
   );
 }
