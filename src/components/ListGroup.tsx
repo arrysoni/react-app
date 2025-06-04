92a9d7112bc9ba5f4561e19c71054dec85cfff15
Aarya Soni <aarya19204@gmail.com>
2025-06-04 12:04:35 +0530

    Building Components: Event Handlers

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,7 @@
-function ListGroup() {
-  // <h1>List</h1>  // We cannot return multiple components in a React Component, the HTML code gets converted to React.createElement('h1'), and it's not allowed.
+// <h1>List</h1>  // We cannot return multiple components in a React Component, the HTML code gets converted to React.createElement('h1'), and it's not allowed.
 
-  // There are multiple ways to solve this issue.
-  /*
+// There are multiple ways to solve this issue.
+/*
     1. Wrap it using a <div> container: But that's like creating another element for the React App
     2. Using Fragments: 
         import { Fragment } from "react";
@@ -11,17 +10,7 @@ function ListGroup() {
     3. Use empty <></>, and delete the import statement. It's a way of telling React that use fragments to wrap the two components. 
     */
 
-  let items = [
-    "New York",
-    "San Francisco",
-    "Philadelphia",
-    "Los Angeles",
-    "Paris",
-  ];
-
-  items = [];
-
-  /* Representing this in the form of a function. 
+/* Representing this in the form of a function. 
   const getMessage = () => {
     items.length === 0 ? <p>No item found</p> : null;
   };
@@ -34,7 +23,7 @@ function ListGroup() {
   If the condition is false, the output will automatically turn false. This is basic computing logic. 
   */
 
-  /*  This is a repetition of the fragment <h1>List</h1> and repetition is not a good practice. So we will use a different way to implement this condition. 
+/*  This is a repetition of the fragment <h1>List</h1> and repetition is not a good practice. So we will use a different way to implement this condition. 
   We get rid of the following if statement, and try rendering this condition inside our jsx component i.e., the return statement.
   Since we cannot use an if statement inside the return statement (illegal in JavaScript), we again wrap the condition in {} and use a ternary operator instead. 
 
@@ -47,15 +36,37 @@ function ListGroup() {
     );
     */
 
-  // We want to render the list now, but since JavaScript doesn't allow for loops,n we use a function called map. We convert one data type into another.
-  // Since the map function isn't allowed in the middle of the code, or under any other component, wrap the map function using '{}'. We can also use a method here as shown in the code.
+// We want to render the list now, but since JavaScript doesn't allow for loops,n we use a function called map. We convert one data type into another.
+// Since the map function isn't allowed in the middle of the code, or under any other component, wrap the map function using '{}'. We can also use a method here as shown in the code.
+
+import { MouseEvent } from "react";
+function ListGroup() {
+  let items = [
+    "New York",
+    "San Francisco",
+    "Philadelphia",
+    "Los Angeles",
+    "Paris",
+  ];
+
+  // For more complex logic, move the logic to a different funtcion for event handling. Since the type of event is not explicitly defined here, we hover over the 'event' in our return statement and get to know the type for itl, i.e., MouseEvent. We therefore, import the MouseEvent.
+  // Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
+
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li> // It is necessary to give each element of the list a unique key, since in our list each sting is unique, we go for key as the item itself.
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            // onClick={() => console.log(item, index)} // When the element of the list is clicked on our webpage, we will see the item, index on the console (Inspect/ Console)
+            onClick={handleClick} // On console, we get the type of the object as SyntheticBaseEvent which is a built in classes in React because different browsers have different implementations of event objects, so to make this cross browser, we have this class SyntheticBaseEvent (a wrapper around the native browser event object)
+          >
+            {item}
+          </li> // It is necessary to give each element of the list a unique key, since in our list each sting is unique, we go for key as the item itself.
         ))}
       </ul>
     </>
